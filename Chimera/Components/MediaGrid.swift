7299385257af7b4e0a2d7425da64b60c81d9bc6e
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2023-01-13 14:16:12 +0100

    Added video test & polished media grid

--- Chimera/Components/MediaGrid.swift
+++ Chimera/Components/MediaGrid.swift
@@ -13,24 +13,18 @@ struct MediaGrid: View {
     ]
     
     private let videos: [VideoMemo] = [
-        VideoMemo(name: "IMG_0684"),
-        VideoMemo(name: "Video")
+        VideoMemo(name: "IMG_0684")
     ]
     
     var body: some View {
-        List {
-            LazyVGrid(columns: columns, spacing: 2) {
-                MediaCard(mediaName: "ConcertImageMemo")
-                MediaCard(mediaName: "ConcertImageMemoVertical")
-                MediaCard(mediaName: "ConcertImageMemo", mediaType: .video(duration: videos[0].mediaDuration))
-                MediaCard(mediaName: "ConcertImageMemoVertical")
-                MediaCard(mediaName: "ConcertImageMemo")
-                MediaCard(mediaName: "ConcertImageMemoVertical", mediaType: .video(duration: videos[1].mediaDuration))
-                MediaCard(mediaName: "ConcertImageMemo")
-            }
-            .listRowInsets(EdgeInsets())
-            .listRowSeparator(.hidden)
-            .listRowBackground(Color.clear)
+        LazyVGrid(columns: columns, spacing: 2) {
+            MediaCard(mediaName: "ConcertImageMemo")
+            MediaCard(mediaName: "ConcertImageMemoVertical")
+            MediaCard(mediaName: "ConcertImageMemo", mediaType: .video(duration: videos[0].mediaDuration))
+            MediaCard(mediaName: "ConcertImageMemoVertical")
+            MediaCard(mediaName: "ConcertImageMemo")
+            MediaCard(mediaName: "ConcertImageMemoVertical", mediaType: .video(duration: videos[0].mediaDuration))
+            MediaCard(mediaName: "ConcertImageMemo")
         }
     }
 }
