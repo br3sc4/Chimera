492205e70e2521a5ad61e9e1cfef7a9e526d2cdd
Antonella Giugliano <giuglianoanton@gmail.com>
2023-01-16 23:55:57 +0100

    add add upcoming event card

--- Chimera/Views/AddUpcomingView.swift
+++ Chimera/Views/AddUpcomingView.swift
@@ -13,7 +13,7 @@ struct AddUpcomingView: View {
     @State private var searchQuery = ""
     var searchResults: [Event] {
         if searchQuery.isEmpty {
-            return []
+            return [events[0], events[3]]
         } else {
             return events.filter { $0.performer.localizedCaseInsensitiveContains(searchQuery)}
         }
@@ -29,19 +29,30 @@ struct AddUpcomingView: View {
                 .padding()
                 
                 if isViaTicketmaster{
-//                    NavigationView{
-                        ScrollView{
-                            ForEach(searchResults){ result in
-                                NavigationLink(destination: EventView(event: result), label: {
-                                    EventCard(image: result.image, performer: result.performer, date: result.date, place: result.place)
+                    //                    NavigationView{
+                    ScrollView{
+                        ForEach(searchResults){ result in
+                            Button(action: {
+                                
+                            }, label: {
+                                HStack{
+                                    Spacer()
+                                    Image(systemName: "plus")
+                                        .fontWeight(.semibold)
+                                    //                                        .font(.system(size: 18))
+                                    Spacer()
+                                    AddUpcomingEventCard(image: result.image, performer: result.performer, date: result.date, place: result.place)
                                         .padding(.vertical)
-                                })
+                                }
                             }
+                            )
+                        }
                             .padding(.horizontal)
-//                        }
+                        //                        }
                     }.searchable(text: $searchQuery)
                 }else {
                     Text("Manually adding")
+                    Spacer()
                 }
             }
             .navigationTitle("Add a new Upcoming Gig")
